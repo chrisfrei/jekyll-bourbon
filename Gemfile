7f41db6ef0be3aac50fb48caa9d55c4a7d95249a
4711 <bzthcyqullt@mailinator.com>
2013-11-14 19:09:49 +0100

    bourbon and neat

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'neat'
+gem 'bourbon'
+
 gem 'jekyll-assets'
 
 # https://github.com/imathis
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,4 +1,7 @@
 require "jekyll-assets"
+require "jekyll-assets/bourbon"
+require "jekyll-assets/neat"
+
 require "jekyll-minify-html"
 require 'jekyll-date-format'
 require 'jekyll-markdown-block'
